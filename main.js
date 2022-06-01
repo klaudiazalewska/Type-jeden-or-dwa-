29e67d4318cd146ac059c2b65af3c98895b59c0e
klaudiazalewska <73396554+klaudiazalewska@users.noreply.github.com>
2022-06-01 20:08:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// Projekt 5 - Zadanie dla Ciebie 
+// Sprawdź czy wpisane w input hasło pasuje do któegoś z hasel w tablicy. Jeśli tak wyświetl wiadomość w <div>
+//wielkość liter nie ma znaczenia (wykorzystaj metodę .toUpperCase lub .toLowerCase - pamiętaj, że metody te działają na pojedyńczym elemencie typu string)
+
+const input = document.querySelector("input");
+const passwords = ['jedEN', 'DwA'];
+const messages = ["super", "działa!"]
+
+const showMessage = (e) => {
+    passwords.forEach((password, index) => {
+        if (password.toLowerCase() === e.target.value.toLowerCase()) {
+            document.querySelector('div').textContent = messages[index];
+        }
+    })
+ // PS. nie skupiaj się na niczym innym w zadaniu niż porównanie i wyświetlenie ( nie rób czyszczenia inputa itp. nie są potrzebne, chyba że masz ochotę)
+}
+
+input.addEventListener("input", showMessage)
+
+
+
